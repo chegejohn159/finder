797338b32ff35287f4fc5be95436c59dae53fec1
kamauchege159 <kamauchege159@gmail.com>
2020-08-03 20:05:09 +0300

    show the array

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("#ufo-table");
+generateTable(table, tableData);
